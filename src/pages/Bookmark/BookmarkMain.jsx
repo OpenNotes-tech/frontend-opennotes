404b6f66e7dfa7e2b9a36003cee8c05e9d790042
Abbosjon Madiev <abbosjon.madiev@gmail.com>
2023-06-07 18:12:11 +0600

    CREATE: Initial Template

---
+++ src/pages/Bookmark/BookmarkMain.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BookmarkMain = () => {
+  return (
+    <div>BookmarkMain</div>
+  )
+}
+
+export default BookmarkMain
---
+++ src/pages/Link/LinkMain.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LinkMain = () => {
+  return (
+    <div>LinkMain</div>
+  )
+}
+
+export default LinkMain

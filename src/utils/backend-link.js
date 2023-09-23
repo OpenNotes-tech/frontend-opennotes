d20190e93d35747250fb83bca7c8768340852aa2
Abbosjon Madiev <abbosjon.madiev@gmail.com>
2023-09-23 22:25:06 +0300

    cors

--- src/utils/backend-link.js
+++ src/utils/backend-link.js
@@ -2,7 +2,7 @@ import axios from "axios";
 // import Cookies from "js-cookie";
 
 export default axios.create({
-  baseURL: `http://localhost:8000/`,
+  baseURL: `https://opennote-b0eb7f2ec0bf.herokuapp.com/`,
   //process.env.REACT_APP_SERVER_PORT
   //https://opennote-b0eb7f2ec0bf.herokuapp.com/
   //http://localhost:8000/
